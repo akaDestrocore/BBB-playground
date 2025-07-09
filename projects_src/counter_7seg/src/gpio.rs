198101decf6159de949a4637a6c01d86756d3acb
akaDestrocore <blackxcat14@yahoo.com>
2025-07-10 01:28:30 +0300

    update `src/gpio.rs` to gpiod wrapper crate for libgpiod

--- projects_src/counter_7seg/src/gpio.rs
+++ projects_src/counter_7seg/src/gpio.rs
@@ -1,81 +1,45 @@
 use std::{
-    fs::{File, OpenOptions},
-    io::{Read, Write},
     path::PathBuf,
     time::Duration,
-    thread::sleep
+    thread::sleep,
+    collections::HashMap,
+    fmt,
 };
 
-/// GPIO character device IOCTL definitions
-const GPIO_GET_CHIPINFO_IOCTL: u32 = 0x8044b401;
-const GPIO_GET_LINEINFO_IOCTL: u32 = 0xc048b402;
-const GPIO_GET_LINEHANDLE_IOCTL: u32 = 0xc16cb403;
-const GPIO_GET_LINEEVENT_IOCTL: u32 = 0xc030b404;
-
-/// GPIO handle flags
-const GPIOHANDLE_REQUEST_INPUT: u32 = 0x01;
-const GPIOHANDLE_REQUEST_OUTPUT: u32 = 0x02;
-const GPIOHANDLE_REQUEST_ACTIVE_LOW: u32 = 0x04;
-const GPIOHANDLE_REQUEST_OPEN_DRAIN: u32 = 0x08;
-const GPIOHANDLE_REQUEST_OPEN_SOURCE: u32 = 0x10;
-
-/// GPIO event flags
-const GPIOEVENT_REQUEST_RISING_EDGE: u32 = 0x01;
-const GPIOEVENT_REQUEST_FALLING_EDGE: u32 = 0x02;
-const GPIOEVENT_REQUEST_BOTH_EDGES: u32 = 0x03;
-
-#[repr(C)]
-struct GpioChipInfo {
-    name: [i8; 32],
-    label: [i8; 32], 
-    lines: u32,
-}
-
-#[repr(C)]
-struct GpioHandleRequest {
-    line_offsets: [u32; 64],
-    flags: u32,
-    default_values: [u8; 64],
-    consumer_label: [i8; 32],
-    lines: u32,
-    fd: i32,
-}
-
-#[repr(C)]
-struct GpioEventRequest {
-    line_offset: u32,
-    handle_flags: u32,
-    event_flags: u32,
-    consumer_label: [i8; 32],
-    fd: i32,
-}
-
-#[repr(C)]
-struct GpioHandleData {
-    values: [u8; 64],
-}
+use gpiod::{
+    Chip,
+    Lines, 
+    Options, 
+    Input, 
+    Output, 
+    Active, 
+    EdgeDetect
+};
 
 #[derive(Debug)]
-pub enum  GpioError {
+pub enum GpioError {
     Io(std::io::Error),
+    NotExported, 
     InvalidValue,
-    ChipNotFound,
-    LineBusy,
-    PermissionDenied,
+    PinNotFound(u16),
+    LineInUse,
 }
 
 pub type GpioResult<T> = Result<T, GpioError>;
 
+#[derive(Clone, Copy)]
 pub enum Direction {
     In,
     Out,
 }
 
+#[derive(Clone, Copy)]
 pub enum Level {
     Low,
     High,
 }
 
+#[derive(Clone, Copy)]
 pub enum Edge {
     None,
     Rising,
@@ -83,55 +47,77 @@ pub enum Edge {
     Both,
 }
 
+#[derive(Clone, Copy)]
 pub enum ActiveLevel {
     Low,
     High,
 }
 
-pub struct Gpio {
-    pin: u16,
-    chip_fd: Option<File>,
-    handle_fd: Option<File>,
-    event_fd: Option<File>,
-    direction: Direction,
-    active_level: ActiveLevel,
+#[derive(Debug, Clone)]
+pub struct PinInfo {
+    pub chip_path: PathBuf,
+    pub line_offset: u32,
+    pub chip_name: String,
+    pub line_name: Option<String>,
 }
 
-impl Gpio {
-    pub fn new(pin: u16, _dir: Direction, _lvl: Level, _edge: Edge) -> GpioResult<Self> {
-        
-        let chip_fd = Self::open_chip(0)?;
+struct GpioChipManager {
+    chips: HashMap<PathBuf, Chip>,
+    pin_map: HashMap<u16, PinInfo>,
+}
 
-        let mut gpio = Self { 
-            pin, 
-            chip_fd: Some(chip_fd), 
-            handle_fd: None, 
-            event_fd: None, 
-            direction: _dir, 
-            active_level: ActiveLevel::High 
+
+impl From<std::io::Error> for GpioError {
+    fn from(err: std::io::Error) -> Self {
+        GpioError::Io(err)
+    }
+}
+
+impl GpioChipManager {
+    fn new() -> GpioResult<Self> {
+        let mut manager: GpioChipManager = Self { 
+            chips = HashMap::new(),
+            pin_map = HashMap::new()
         };
 
-        gpio.set_direction(_dir)?;
-        if matches!(_dir, Direction::Out) {
-            gpio.set_value(_lvl)?;
-        } 
+        manager.discover_chips()?;
+        Ok(manager)
+    }
 
-        if !matches!(_edge, Edge::None) {
-            gpio.set_edge(_edge)?;    
+   fn discover_chips(&mut self) -> GpioResult<()> {
+        let chip_paths = Chip::list_devices()?;
+        let mut current_pin = 0u16;
+        
+        for path in chip_paths {
+            let chip = Chip::new(&path)?;
+            let num_lines = chip.num_lines();
+            
+            // Map lines to a corresponding pin numbers
+            for line_offset in 0..num_lines {
+                let line_info = chip.line_info(line_offset)?;
+                let pin_info = PinInfo {
+                    chip_path: path.clone(),
+                    line_offset,
+                    chip_name: chip.name().to_string(),
+                    line_name: if line_info.name.is_empty() {
+                        None
+                    } else {
+                        Some(line_info.name.clone())
+                    },
+                };
+                
+                self.pin_map.insert(current_pin, pin_info);
+                current_pin += 1;
+            }
+            
+            self.chips.insert(path, chip);
         }
-
-        Ok(gpio)
-    }
-
-    pub fn set_direction(&mut self, _dir: Direction) -> GpioResult<()> {
+        
         Ok(())
     }
 
-    pub fn set_value(&mut self, _lvl: Level) -> GpioResult<()> {
-        Ok(())
+    fn get_pin_info(&self, pin: u16) -> GpioResult<&PinInfo> {
+        self.pin_map.get(&pin).ok_or(GpioError::PinNotFound(pin))
     }
 
-    pub fn set_edge(&mut self, _edge: Edge) -> GpioResult<()> {
-        Ok(())
-    }
 }
